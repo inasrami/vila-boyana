a14f8036e9d35e7d002989d10169b075a9d04c3d
Inas Rami <inkorami27@gmail.com>
2026-02-26 22:27:28 +0200

    Refactor AOS initialization to App.vue; update Vite base path for improved deployment compatibility

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,18 @@
 <script setup>
+import { onMounted } from 'vue';
 import navbar from './components/navbar.vue';
 import Footer from './components/Footer.vue';
+import AOS from 'aos';
+import 'aos/dist/aos.css';
+
+
+onMounted(() => {
+  AOS.init({
+    duration: 1000,
+    once: true,
+    offset: 100
+  });
+});
 </script>
 
 <template>
